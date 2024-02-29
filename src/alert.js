e78884ef624d3835689e7466cabe006ca53afdd8
AshleySantana <santana02ashley@gmail.com>
2024-02-29 12:11:07 -0500

    change

--- src/alert.js
+++ src/alert.js
@@ -3,11 +3,14 @@ import { LitElement, html, css } from 'lit';
 export class Alert extends LitElement {
 
   static get tag() {
-    return 'alert';
+    return 'alert-tag';
   }
 
   constructor() {
     super();
+      this.header = "Alert";
+      this.date = "1/1/2001";
+      this.body = "Alert body";
 
      }
 
@@ -20,20 +23,29 @@ export class Alert extends LitElement {
 
   render() {
     return html`
-
-      <div class="alert-wrapper"></div>
-      
-        <div class="alerts-content-wrap>
-          <p>date</p>
-          <p>alert message</p>
+      <div class="alert-wrapper"> 
+        <div class="minimized-alert"> 
+          <button class="expand-alert">expland alert</button>
+          <p class="alert-header">${this.header}</p>
         </div>
-      
-
+        <div class="maximized-alert">
+          <p class="date">${this.date}</p>
+          <p class="alert-header">${this.header}</p>
+          <div class="alert-message">
+            <slot></slot>
+          </div>
+          <button class="minimize-alert">minimize alert</button>
+        </div>
+      </div>
       `;
     }
 
   static get properties() {
     return {
+      header: {type: String},
+      date: {type: String},
+      
+
 
     };
   }
