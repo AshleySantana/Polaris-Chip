49dc8c249606ac9d22f07df935f5d717e9571c67
AshleySantana <santana02ashley@gmail.com>
2024-03-21 12:08:41 -0400

    change

---
+++ src/haxcms-party-ui.js
@@ -0,0 +1,35 @@
+import { LitElement, html, css } from 'lit';
+
+export class HaxcmsPartyUi extends LitElement {
+
+  static get tag() {
+    return 'haxcms-party-ui';
+  }
+
+  constructor() {
+    super();
+
+    }
+
+  static get styles() {
+    return css`
+      
+
+    `;
+    }
+
+  render() {
+    return html`
+    
+    `;
+    }
+
+  static get properties() {
+    return {
+      
+      
+    };
+  }
+}
+
+globalThis.customElements.define(HaxcmsPartyUi.tag, HaxcmsPartyUi);
